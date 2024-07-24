d002c1923c297e634dc1a9dad2f117c94fde509d
Nigithasivanand <150169179+Nigithasivanand@users.noreply.github.com>
2024-07-24 09:13:06 +0530

    Add files via upload

---
+++ Day2/src/Login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Login({ setIsRegistered }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    alert('Logged in!');
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login </h2>
+      <p>Welcome Back!👋</p>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      <p>
+        Don't have an account? <span onClick={() => setIsRegistered(false)}>Register here</span>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
